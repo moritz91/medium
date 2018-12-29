82cf0a915dd84cb1d1a87d22e1cfa7c2b24b3e2c
moritz91 <moritz.giller@hotmail.com>
2018-12-29 17:51:01 +0100

    first

---
+++ packages/web/pages/home.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Button } from "@codeponder/ui";
+
+export default class Home extends React.Component {
+  render() {
+    return <Button>hello from ui package</Button>;
+  }
+}
