fa5179d50c9e53c0de458611af1fad2569b39fb0
moritz91 <moritz.giller@hotmail.com>
2019-01-23 01:43:42 +0100

    change

--- packages/web/pages/home.tsx
+++ packages/web/pages/home.tsx
@@ -6,7 +6,6 @@ export default class Home extends React.Component {
     return (
       <a href="http://localhost:4000/auth/github">
         <Button>login with github</Button>
-        <div>Test</div>
       </a>
     );
   }
