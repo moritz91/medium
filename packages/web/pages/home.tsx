1690330f71578bd6b3571a2209ee823738e5c3c6
moritz91 <moritz.giller@hotmail.com>
2019-01-28 22:54:21 +0100

    links

--- packages/web/pages/home.tsx
+++ packages/web/pages/home.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
 import { MyButton } from "@medium/ui";
+import Link from "next/link";
 
 export default class Home extends React.Component {
   render() {
     return (
-      <a href="http://localhost:4000/auth/github">
-        <MyButton variant="primary">login with github</MyButton>
-      </a>
+      <div>
+        <a href="http://localhost:4000/auth/github">
+          <MyButton variant="primary">login with github</MyButton>
+        </a>
+        <Link as={"posts"}>
+          <MyButton variant="primary">
+            <a>Posts</a>
+          </MyButton>
+        </Link>
+      </div>
     );
   }
 }
