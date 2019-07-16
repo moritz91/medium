7e13791b397c858ff4b94db3a21091c5310bd7f7
moritz91 <moritz.giller@hotmail.com>
2019-07-16 01:01:22 -0400

    added copy link logic

--- packages/web/modules/comment/copyLink.tsx
+++ packages/web/modules/comment/copyLink.tsx
@@ -1,109 +1,33 @@
-import { get } from "lodash";
-import React, { useContext } from "react";
+import React from "react";
 import { Text } from "rebass";
-import {
-  DeleteCommentComponent,
-  GetCommentsByIdQuery,
-  GetCommentsByIdVariables,
-  MeComponent
-} from "../../components/apollo-components";
 import { Button } from "../../components/button";
-import {
-  FlyoutContext,
-  FlyoutContextProps
-} from "../../components/context/FlyoutContext";
-import { PostContext } from "../../components/context/PostContext";
 import { Icon } from "../../components/icon";
-import { getCommentsByIdQuery } from "../../graphql/comment/query/getCommentsById";
+import { useContext } from "react";
+import { FlyoutContext } from "../../components/context/FlyoutContext";
+import Router from "next/router";
 
 interface Props {
   commentId: string;
 }
 
 export const CopyLink = ({ commentId }: Props) => {
-  const { postingId } = useContext(PostContext);
-  const { dispatch } = useContext<FlyoutContextProps>(FlyoutContext);
+  const { dispatch } = useContext(FlyoutContext);
+  const { query } = Router;
+  const route = `localhost:3000/p/${query!.id}#${commentId}`;
 
   return (
-    <DeleteCommentComponent>
-      {mutate => (
-        <>
-          <MeComponent variables={{ withBookmarks: false }}>
-            {({ data, loading }) => {
-              if (loading) {
-                return null;
-              }
-
-              let isLoggedIn = !!get(data, "me", false);
-
-              if (data && data.me && isLoggedIn) {
-                return (
-                  <Button
-                    variant="action"
-                    style={{ display: "flex" }}
-                    key={commentId}
-                    onClick={async () => {
-                      const response = await mutate({
-                        variables: {
-                          id: commentId
-                        },
-                        update: (cache, { data }) => {
-                          if (!data) {
-                            return;
-                          }
-
-                          const x = cache.readQuery<
-                            GetCommentsByIdQuery,
-                            GetCommentsByIdVariables
-                          >({
-                            query: getCommentsByIdQuery,
-                            variables: {
-                              input: { postingId }
-                            }
-                          });
-
-                          cache.writeQuery<
-                            GetCommentsByIdQuery,
-                            GetCommentsByIdVariables
-                          >({
-                            query: getCommentsByIdQuery,
-                            variables: {
-                              input: { postingId }
-                            },
-                            data: {
-                              __typename: "Query",
-                              findCommentsById: {
-                                __typename: "FindCommentResponse",
-                                comments: [
-                                  ...x!.findCommentsById.comments.filter(
-                                    c => c.id !== commentId
-                                  )
-                                ],
-                                hasMore: false
-                              }
-                            }
-                          });
-                        }
-                      });
-
-                      if (response) {
-                        dispatch({
-                          type: "closeFlyout"
-                        });
-                      }
-                    }}
-                  >
-                    <Icon size={16} fill="#5C6AC4" name={"link"} />
-                    <Text ml={2}>Copy Link</Text>
-                  </Button>
-                );
-              }
-
-              return null;
-            }}
-          </MeComponent>
-        </>
-      )}
-    </DeleteCommentComponent>
+    <Button
+      variant="action"
+      style={{ display: "flex" }}
+      key={commentId}
+      onClick={async () => {
+        navigator.clipboard
+          .writeText(route)
+          .then(() => dispatch({ type: "closeFlyout" }));
+      }}
+    >
+      <Icon size={16} fill="#5C6AC4" name={"link"} />
+      <Text ml={2}>Copy Link</Text>
+    </Button>
   );
 };
